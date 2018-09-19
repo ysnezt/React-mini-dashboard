a6dab4441dc2098afd168d498a1fcf00376b9964
yasinezt <yasin_lord2009@yahoo.com>
2018-09-19 19:15:11 +0330

    modified webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-    // Change to your "entry-point".
+    
     entry: './src/index',
     output: {
         path: path.resolve(__dirname, 'public/dist'),
