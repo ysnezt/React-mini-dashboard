ac0d2922d18fa9d467db7301133b1b76fd30e941
yasinezt <yasin_lord2009@yahoo.com>
2018-09-19 19:12:51 +0330

    adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+    // Change to your "entry-point".
+    entry: './src/index',
+    output: {
+        path: path.resolve(__dirname, 'public/dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './public'
+          },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+        rules: [{
+            // Include ts, tsx, and js files.
+            test: /\.(tsx?)|(js)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }],
+    },
+    mode: 'none',
+    watch: false,
+};
